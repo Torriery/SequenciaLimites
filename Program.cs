11dbebcfbda0882ba615b05d4192ba2e2e8feac0
torrieri <andreastorrieri3@gmail.com>
2022-05-20 22:36:45 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("bote algum numero: ");
+int inicio = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("bote outro numero :");
+int fim = Convert.ToInt32(Console.ReadLine());
+
+if (fim >= inicio)
+{
+    int n = inicio;
+
+    while (n <= fim)
+    {
+        Console.Write($"{n} ");
+        n += 1;
+    }
+}
+else
+{
+    Console.WriteLine("Não é possível terminar antes de iniciar.");
+}
